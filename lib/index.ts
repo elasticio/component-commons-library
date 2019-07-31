b27c6965789753f8f47e63908c404c7e2e8ea13e
ElBabbler <gladikov.denis@gmail.com>
2019-07-31 15:11:15 +0300

    Adding Rest Client classes for auth: - No auth - Basic - API Key - OAuth2

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,11 @@
 import { convertJsonSchemaToEioSchema, makeSchemaInline } from './jsonSchema/jsonSchemaConversionUtil';
 import { getLogger } from './logger/logger';
 
+export { ApiKeyRestClient } from './authentication/ApiKeyRestClient';
+export { BasicAuthRestClient } from './authentication/BasicAuthRestClient';
+export { NoAuthRestClient } from './authentication/NoAuthRestClient';
+export { OAuth2RestClient } from './authentication/OAuth2AuthorizationCodeRestClient';
+
 export class JsonSchema {
   static convertJsonSchemaToEioSchema = convertJsonSchemaToEioSchema;
   static makeSchemaInline = makeSchemaInline;
---
+++ test/authentication/ApiKeyRestClient.spec.ts
@@ -0,0 +1,87 @@
+import chai from 'chai';
+import nock from 'nock';
+import sinon from 'sinon';
+
+const { expect } = chai;
+const { ApiKeyRestClient } = require('../../lib/index');
+
+let options;
+let emitter;
+const url = 'https://example.com';
+const resourceServerUrl = 'https://resourceServerUrl.com';
+const successStatusCode = 200;
+const notFoundStatusCode = 404;
+const successBody = 'Ok';
+const notFoundBody = 'Not found';
+const errNotFound = `Error in making request to ${url}/ Status code: ${notFoundStatusCode}, Body: "${notFoundBody}"`;
+let cfg;
+
+describe('ApiKeyRestClient', () => {
+  let client;
+  let reqheaders;
+
+  beforeEach(() => {
+    options = {
+      url,
+      method: 'GET',
+      body: {},
+      headers: {},
+    };
+    emitter = {
+      emit: sinon.spy(),
+      logger: {
+        error: sinon.spy(),
+        warn: sinon.spy(),
+        info: sinon.spy(),
+        debug: sinon.spy(),
+        trace: sinon.spy(),
+      },
+    };
+
+    cfg = {
+      resourceServerUrl,
+      apiKeyHeaderName: 'apiHeader',
+      apiKeyHeaderValue: 'apiKey',
+    };
+
+    client = new ApiKeyRestClient(emitter, cfg);
+    reqheaders = {
+      apiHeader: 'apiKey',
+    };
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('Should succeed makeRequest method, urlIsSegment: false', async () => {
+    options.urlIsSegment = false;
+    nock(url, { reqheaders })
+      .get('/')
+      .reply(successStatusCode, successBody);
+    const result = await client.makeRequest(options);
+    expect(result).to.be.deep.equal(successBody);
+  });
+
+  it('Should succeed makeRequest method, urlIsSegment: true', async () => {
+    nock(resourceServerUrl, { reqheaders })
+      .get(`/${url}`)
+      .reply(successStatusCode, successBody);
+    const result = await client.makeRequest(options);
+    expect(result).to.be.deep.equal(successBody);
+  });
+
+  it('Should fail, 400', async () => {
+    options.urlIsSegment = false;
+    nock(url, { reqheaders })
+      .get('/')
+      .reply(notFoundStatusCode, notFoundBody);
+    await client.makeRequest(options)
+      .then(() => {
+        throw new Error('Test case does not expect success response');
+      })
+      .catch((e) => {
+        expect(e.message).to.be.equal(errNotFound);
+      });
+  });
+});
