d99b10e019b42e6ae686619f286f30d71522977b
Hannah Bulmer <hannah.bulmer@elastic.io>
2019-10-16 17:20:03 +0200

    Fix test

--- test/authentication/OAuth2AuthorizationCodeRestClient.spec.ts
+++ test/authentication/OAuth2AuthorizationCodeRestClient.spec.ts
@@ -58,7 +58,7 @@ describe('OAuth2AuthorizationCodeRestClient', () => {
       .reply(successStatusCode, successBody);
     const result = await client.makeRequest(options);
     expect(result).to.be.deep.equal(successBody);
-    expect(emitter.emit.withArgs('updateKeys').callCount).to.be.equal(1);
+    expect(emitter.emit.withArgs('updateKeys').callCount).to.be.equal(0);
   });
 
   it('Should succeed, urlIsSegment: false', async () => {
