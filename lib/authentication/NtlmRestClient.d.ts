d2e1d75e76d6f60d4f43cb9f86896c924cbfb004
uaArsen <arsenzhd@gmail.com>
2020-03-25 13:28:23 +0200

    Fix node version dependency

---
+++ lib/authentication/NtlmRestClient.d.ts
@@ -0,0 +1,5 @@
+import { NoAuthRestClient } from './NoAuthRestClient';
+export declare class NtlmRestClient extends NoAuthRestClient {
+    constructor(emitter: any, cfg: any);
+}
+//# sourceMappingURL=NtlmRestClient.d.ts.map
---
+++ lib/jsonataTransform/jsonataTransform.d.ts
@@ -0,0 +1,9 @@
+/**
+ * This method will be called from elastic.io platform providing following data
+ *
+ * @param msg incoming message object that contains ``body`` with payload
+ * @param cfg configuration that is account information and configuration field values
+ * @param context this of action or trigger, optional
+ */
+export declare function jsonataTransform(msg: any, cfg: any, context: any): any;
+//# sourceMappingURL=jsonataTransform.d.ts.map
