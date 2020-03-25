90ccb02619403c80801fb6efb2b147d82fd89bab
uaArsen <arsenzhd@gmail.com>
2020-03-25 13:35:02 +0200

    clean trash

--- lib/authentication/NtlmRestClient.d.ts
+++
@@ -1,5 +0,0 @@
-import { NoAuthRestClient } from './NoAuthRestClient';
-export declare class NtlmRestClient extends NoAuthRestClient {
-    constructor(emitter: any, cfg: any);
-}
-//# sourceMappingURL=NtlmRestClient.d.ts.map
