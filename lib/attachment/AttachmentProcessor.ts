dc3dbef11b25b4441cb90a116a6d093533f73b1b
Ilya <soroka14021998@gmail.com>
2021-07-30 11:00:04 +0300

    Added maester-implementation for getAttachment, maxContentLength fix (#46)
    * Fixed bug with misspelling maxContentLength and maxBodyLenth of request config
    * added maester implementation for getAttachment

--- lib/attachment/AttachmentProcessor.ts
+++ lib/attachment/AttachmentProcessor.ts
@@ -1,17 +1,22 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { URL } from 'url';
+import { StorageClient, ObjectStorage } from '@elastic.io/maester-client/dist';
 const restNodeClient = require('elasticio-rest-node')();
 
+export const STORAGE_TYPE_PARAMETER = 'storage_type';
+export const DEFAULT_STORAGE_TYPE = 'steward';
+export const MAESTER_OBJECT_ID_ENDPOINT = '/objects/';
+const { ELASTICIO_OBJECT_STORAGE_TOKEN = '' , ELASTICIO_OBJECT_STORAGE_URI = '' } = process.env;
+const maesterCreds = { jwtSecret: ELASTICIO_OBJECT_STORAGE_TOKEN, uri: ELASTICIO_OBJECT_STORAGE_URI };
 const REQUEST_TIMEOUT = process.env.REQUEST_TIMEOUT ? parseInt(process.env.REQUEST_TIMEOUT, 10) : 10000; // 10s
 const REQUEST_MAX_RETRY = process.env.REQUEST_MAX_RETRY ? parseInt(process.env.REQUEST_MAX_RETRY, 10) : 7; // 10s
 const REQUEST_RETRY_DELAY = process.env.REQUEST_RETRY_DELAY ? parseInt(process.env.REQUEST_RETRY_DELAY, 10) : 7000; // 7s
-const REQUEST_MAX_CONTENT_LENGTH = process.env.REQUEST_MAX_CONTENT_LENGTH ? parseInt(process.env.REQUEST_MAX_CONTENT_LENGTH, 10) : 10485760; // 10MB
+const REQUEST_MAX_BODY_LENGTH = process.env.REQUEST_MAX_BODY_LENGTH ? parseInt(process.env.REQUEST_MAX_BODY_LENGTH, 10) : 10485760; // 10MB
 
 export class AttachmentProcessor {
 
   async getAttachment(url: string, responseType: string) {
-    const ax = axios.create();
-    AttachmentProcessor.addRetryCountInterceptorToAxios(ax);
-
+    const storageType = AttachmentProcessor.getStorageTypeByUrl(url);
     const axConfig = {
       url,
       responseType,
@@ -21,7 +26,11 @@ export class AttachmentProcessor {
       delay: REQUEST_RETRY_DELAY,
     } as AxiosRequestConfig;
 
-    return ax(axConfig);
+    switch (storageType) {
+      case 'steward': return AttachmentProcessor.getStewardAttachment(axConfig);
+      case 'maester': return AttachmentProcessor.getMaesterAttachment(axConfig);
+      default: throw new Error(`Storage type "${storageType}" is not supported`);
+    }
   }
 
   async uploadAttachment(body) {
@@ -36,7 +45,7 @@ export class AttachmentProcessor {
       timeout: REQUEST_TIMEOUT,
       retry: REQUEST_MAX_RETRY,
       delay: REQUEST_RETRY_DELAY,
-      maxContentLength: REQUEST_MAX_CONTENT_LENGTH,
+      maxBodyLength: REQUEST_MAX_BODY_LENGTH,
     } as AxiosRequestConfig;
 
     return ax(axConfig);
@@ -45,6 +54,35 @@ export class AttachmentProcessor {
   static async preparePutUrl() {
     const signedUrl = await restNodeClient.resources.storage.createSignedUrl();
     return signedUrl.put_url;
+  }
+
+  static async getStewardAttachment(axConfig) {
+    const ax = axios.create();
+    AttachmentProcessor.addRetryCountInterceptorToAxios(ax);
+    return ax(axConfig);
+  }
+
+  static async getMaesterAttachment(axConfig) {
+    const client = new StorageClient(maesterCreds);
+    const objectStorage = new ObjectStorage(maesterCreds, client);
+    const maesterAttachmentId = AttachmentProcessor.getMaesterAttachmentIdByUrl(axConfig.url);
+    const response = await objectStorage.getById(maesterAttachmentId, axConfig.responseType);
+    return { data: response };
+  }
+
+  static getStorageTypeByUrl(urlString) {
+    const url = new URL(urlString);
+    const storageType = url.searchParams.get(STORAGE_TYPE_PARAMETER);
+    return storageType || DEFAULT_STORAGE_TYPE;
+  }
+
+  static getMaesterAttachmentIdByUrl(urlString): string {
+    const { pathname } = new URL(urlString);
+    const maesterAttachmentId = pathname.split(MAESTER_OBJECT_ID_ENDPOINT)[1];
+    if (!maesterAttachmentId) {
+      throw new Error('Invalid Maester Endpoint');
+    }
+    return maesterAttachmentId;
   }
 
   static addRetryCountInterceptorToAxios(ax) {
