5f9aabd71355a93ec4775855dfaa1fee1b1d3150
Olha Virolainen <olga.shukel@gmail.com>
2022-11-29 17:26:24 +0200

    add retryOptions (#81)
    add retryOptions

--- spec/attachment/AttachmentProcessor.spec.ts
+++ spec/attachment/AttachmentProcessor.spec.ts
@@ -19,8 +19,16 @@ const formStream = (dataString: string): Readable => {
 };
 
 describe('AttachmentProcessor', () => {
+  const getAttachment = async () => Readable.from(formStream('i`m a stream'));
+  const customRetryOptions = {
+    requestTimeout: 1000,
+    retriesCount: 5
+  };
   const attachmentProcessor = new AttachmentProcessor('userAgent');
   describe('Steward', () => {
+    afterEach(() => {
+      sinon.restore();
+    });
     it('Should successfully retrieve csv', async () => {
       const attachmentOptions = {
         'content-type': 'arraybuffer',
@@ -28,7 +36,7 @@ describe('AttachmentProcessor', () => {
       };
 
       const result: any = await attachmentProcessor.getAttachment(attachmentOptions.url, attachmentOptions['content-type']);
-      const encodedResult = new Buffer(result.data, 'binary').toString('base64');
+      const encodedResult = Buffer.from(result.data, 'binary').toString('base64');
       const expectedResult = fs.readFileSync('spec/attachment/resources/base64csv.txt').toString();
       expect(encodedResult).to.be.equal(expectedResult);
     });
@@ -39,15 +47,36 @@ describe('AttachmentProcessor', () => {
       };
 
       const result: any = await attachmentProcessor.getAttachment(attachmentOptions.url, 'arraybuffer');
-      const encodedResult = new Buffer(result.data, 'binary').toString('base64');
+      const encodedResult = Buffer.from(result.data, 'binary').toString('base64');
       const expectedResult = fs.readFileSync('spec/attachment/resources/base64Png.txt').toString();
       expect(encodedResult).to.be.equal(expectedResult);
+    });
+
+    it('Should successfully getAttachment with custom RetryOptions', async () => {
+      const getStewardAttachment = sinon.stub(AttachmentProcessor.prototype, <any>'getStewardAttachment').callsFake(async () => ({ data: formStream('i`m a stream') }));
+      const attachmentOptions = {
+        'content-type': 'stream',
+        url: 'https://example.com',
+      };
+
+      const result: any = await attachmentProcessor.getAttachment(attachmentOptions.url, attachmentOptions['content-type'], customRetryOptions);
+      expect(result.toString('base64')).to.be.equal({ data: formStream('i`m a stream') }.toString());
+      expect(getStewardAttachment.getCall(0).args[0]).to.deep.equal({
+        url: 'https://example.com',
+        responseType: 'stream',
+        method: 'get',
+        timeout: customRetryOptions.requestTimeout,
+        retry: customRetryOptions.retriesCount
+      });
     });
   });
   describe('maester', () => {
     let getById;
+    let addAttachment;
+
     beforeEach(() => {
       getById = sinon.stub(ObjectStorage.prototype, 'getOne').callsFake(async () => ({ data: formStream('i`m a stream') }));
+      addAttachment = sinon.stub(ObjectStorage.prototype, 'add').callsFake(async () => ('attachmentId'));
     });
     afterEach(() => {
       sinon.restore();
@@ -62,6 +91,21 @@ describe('AttachmentProcessor', () => {
       expect(result.toString('base64')).to.be.equal({ data: formStream('i`m a stream') }.toString());
       expect(getById.getCall(0).args[0]).to.be.equal('object_id');
       expect(getById.getCall(0).args[1]).to.be.deep.equal({ responseType: 'stream' });
+    });
+
+    it('Should successfully upload attachment with default RetryOptions', async () => {
+      const result: string = await attachmentProcessor.uploadAttachment(getAttachment);
+      expect(result).to.be.deep.equal('attachmentId');
+      expect(addAttachment.getCall(0).args[1].retryOptions).to.deep.equal({
+        requestTimeout: 20000,
+        retriesCount: 2
+      });
+    });
+
+    it('Should successfully upload attachment with custom RetryOptions', async () => {
+      const result: string = await attachmentProcessor.uploadAttachment(getAttachment, customRetryOptions);
+      expect(result).to.be.deep.equal('attachmentId');
+      expect(addAttachment.getCall(0).args[1].retryOptions).to.deep.equal(customRetryOptions);
     });
   });
 });
